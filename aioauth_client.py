0f0fc083ae4c28ad17d2552b115d4c344f94df06
Kirill Klenov <horneds@gmail.com>
2018-10-05 18:31:39 +0300

    Bump version: 0.12.2 → 0.12.3

--- aioauth_client.py
+++ aioauth_client.py
@@ -15,7 +15,7 @@ import yarl
 from aiohttp import BasicAuth, web
 
 
-__version__ = "0.12.2"
+__version__ = "0.12.3"
 __project__ = "aioauth-client"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
