13c9e40dc0f28a7c0a5f5e8cd24022f08b78dfc7
Kirill Klenov <horneds@gmail.com>
2020-03-18 12:11:12 +0300

    build(version): 0.18.2 -> 0.18.3

--- aioauth_client.py
+++ aioauth_client.py
@@ -14,7 +14,7 @@ import yarl
 from aiohttp import web
 
 
-__version__ = "0.18.2"
+__version__ = "0.18.3"
 __project__ = "aioauth-client"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
