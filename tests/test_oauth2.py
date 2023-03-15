89e9a9a99efd39f97c5be7fb31050f583c194a12
konstantin <konstantin.klein@hochfrequenz.de>
2023-03-15 13:06:21 +0100

    ✔ Add unit test to reproduce the issue

--- tests/test_oauth2.py
+++ tests/test_oauth2.py
@@ -1,4 +1,5 @@
 async def test_oauth2(http, response):
+async def test_oauth2(http, response, caplog):
     from aioauth_client import GithubClient, ClientRegistry
 
     github = GithubClient(client_id='cid', client_secret='csecret')
@@ -6,12 +7,16 @@ async def test_oauth2(http, response):
     assert 'github' in ClientRegistry.clients
     assert github.get_authorize_url() == 'https://github.com/login/oauth/authorize?client_id=cid&response_type=code'  # noqa
 
+    caplog.set_level(logging.DEBUG, logger="OAuth: github")
     http.return_value = response(json={'access_token': 'TEST-TOKEN'})
     token, meta = await github.get_access_token('000')
     assert token == 'TEST-TOKEN'
     assert meta
     assert http.called
-
+    # check that no credentials have been logged
+    # https://github.com/klen/aioauth-client/issues/191
+    assert not any(msg for msg in caplog.messages if "TEST-TOKEN" in msg)
+    assert not any(msg for msg in caplog.messages if "csecret" in msg)
     http.reset_mock()
     http.return_value = response(json={'access_token': 'TEST-TOKEN'})
 
